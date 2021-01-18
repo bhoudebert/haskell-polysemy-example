5fd9f7109ecf7668a43b55c73567d9219d94501f
bhoudebert <bhoudebert@gmail.com>
2021-01-18 09:11:25 +0100

    poly

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -4,14 +4,8 @@ import Lib
 
 import Test.Hspec
 
-import Test.QuickCheck
-import Test.QuickCheck.Instances ()
-
-import GHC.IO (liftIO)
-
-import Generic.Random
-import Test.QuickCheck.Monadic
 import Polysemy
+import Polysemy.Writer
 
 questionMeToIOMocked :: Member (Embed IO) r => Sem (QuestionMe ': r) a -> Sem r a
 questionMeToIOMocked = interpret \case
@@ -22,6 +16,19 @@ questionMeToIOMocked = interpret \case
 giveTitleToIOMocked :: Member (Embed IO) r => Sem (GiveTitle ': r) a -> Sem r a
 giveTitleToIOMocked = interpret (\(BeKing _) -> return "<CROWD>:   Hail to the Enchanter!")
 
+-- Records
+
+questionMeToRecord :: Member (Writer [String]) r => Sem (QuestionMe ': r) a -> Sem r a
+questionMeToRecord = interpret \case
+  LogQuestion q -> tell ["count" <> q]
+  LogAnswer n -> tell ["count" <> n]
+
+giveTitleToRecord :: Member (Writer [String]) r => Sem (GiveTitle ': r) a -> Sem r a
+giveTitleToRecord = interpret \case
+  BeKing n -> do 
+    tell ["Enchanter" <> n]
+    return "records counted"
+
 -- | Everything is happening at interpreter level so ... we can do everything now!
 spec :: Spec
 spec = do
@@ -29,3 +36,8 @@ spec = do
     it "Should be a complete different logging from Main" $ do
       title <- runM . giveTitleToIOMocked. questionMeToIOMocked $ displayIt "Ben"
       title `shouldBe` "<CROWD>:   Hail to the Enchanter!" 
+    it "There should be 3 calls to mocks (@see record)" $
+      let (list, result) = Polysemy.run . runWriter . giveTitleToRecord . questionMeToRecord $ displayIt "Ben"
+      in do 
+        length list `shouldBe` 3 
+        result `shouldBe` "records counted"
